0b47fda3317ad0abe4e862904722e411cb5fcf20
Brandon Wood <57229204+brandonkeithwood004@users.noreply.github.com>
2020-08-01 10:42:08 -0500

    third

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
 @app.route('/')
-def home():
-    return "Hey there!"
+def index():
+    return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True)
