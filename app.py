4bb2f923bbb614040b7a1c4eecf8a835f8280a0b
Brandon Wood <57229204+brandonkeithwood004@users.noreply.github.com>
2020-08-01 11:34:55 -0500

    make it better

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
