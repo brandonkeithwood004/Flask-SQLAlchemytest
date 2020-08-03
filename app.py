c734b897e50fae940e4b5cd7b2f08a79e0ce65aa
Brandon Wood <57229204+brandonkeithwood004@users.noreply.github.com>
2020-08-03 12:01:49 -0500

    end

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, url_for
+from flask import Flask,render_template, url_for, request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,9 +16,54 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/',methods={'POST','GET'})
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+        @app.route('/delete/<int:id')
+        def delete(id):
+            task_to_delete = Todo.query.get_or404(id)
+
+            try:
+                db.session.delete(task_to_delete)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return 'There was a problem deleting that task'
+
+                @app.route('/update/<int:id>', methods=['GET','POST'])
+                def update(id):
+                    task = Todo.query.get_or404(id)
+                    if request.method == 'POST':
+                       
+                     task.content = request.form['content']
+                    
+                    try:
+                        db.session.commit()
+                        return redirect('/')
+                    except:
+                            return 'There was an issue updating your task'
+                    
+                    else:
+                        return render_template('update.html', task=task)
+
+
+
+
 
 
 if __name__ == "__main__":
